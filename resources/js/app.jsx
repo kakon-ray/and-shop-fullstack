4665a80d7952c7fbc3c4c02f9107720b1a28bbba
unknown <thisiskakonray@gmail.com>
2024-01-28 14:00:31 +0600

    navbar and banner font change

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import '../css/app.css'
 
 createInertiaApp({
   resolve: name => {
